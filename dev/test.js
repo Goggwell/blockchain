0de0076aaa94d5e5781d4e3698307aa2b7cd5ed6
Samuel Yusuf <zulkarnaensamuel1@gmail.com>
2021-09-22 17:06:47 +0700

    'finalizing'

--- dev/test.js
+++ dev/test.js
@@ -1,26 +1,4 @@
 const Blockchain = require('./blockchain');
-const bitcoin = new Blockchain();
+const testcoin = new Blockchain();
 
-const previousBlockHash = 'OIOIOSDAIOIWIE';
-const currentBlockData = [
-    {
-        amount: 10,
-        sender: 'N9901N',
-        recipient: 'M0NG0L1A'
-    },
-    {
-        amount: 30,
-        sender: 'G425ML',
-        recipient: 'ST3V3'
-    },
-    {
-        amount: 200,
-        sender: 'HML51',
-        recipient: 'B0NN3T'
-    }
-];
-const nonce = 100;
-
-console.log(bitcoin.proofOfWork(previousBlockHash, currentBlockData));
-
-console.log(bitcoin.hashBlock(previousBlockHash, currentBlockData, 31814));
+// test blockchain.js here
